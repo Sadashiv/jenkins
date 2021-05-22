b63678f6f1252ed95c7e13ac322c191e2da03d05
Sadashiv Badagi <sadamsrit@gmail.com>
2021-05-22 16:30:27 +0530

    Added maven url

--- maven.groovy
+++ maven.groovy
@@ -1,25 +1,19 @@
 def owner = 'mesosphere'
 def project = 'jenkins-mesos'
-def branchApi = new URL("https://api.github.com/repos/${owner}/${project}/branches")
-def branches = new groovy.json.JsonSlurper().parse(branchApi.newReader())
-branches.each {
-  def branchName = it.name
-  def jobName = "${owner}-${project}-${branchName}".replaceAll('/','-')
-  job(jobName) {
-    scm {
-        git {
-            remote {
-              github("${owner}/${project}")
-            }
-            branch("${branchName}")
-            createTag(false)
-        }
-    }
-    triggers {
-        scm('*/15 * * * *')
-    }
-    steps {
-        shell('ls -l')
-    }
+def jobName = "${owner}-${project}".replaceAll('/','-')
+job(jobName) {
+  scm {
+      git {
+          remote {
+            github("${owner}/${project}")
+          }
+          createTag(false)
+      }
+  }
+  triggers {
+      scm('*/15 * * * *')
+  }
+  steps {
+      shell('ls -l')
   }
 }
