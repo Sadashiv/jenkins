cd5bcc972467957c1f0f254452e49097031ea5e8
Sadashiv Badagi <sadamsrit@gmail.com>
2021-05-23 16:54:45 +0530

    Added agent

--- pipeline_maven.groovy
+++ pipeline_maven.groovy
@@ -4,13 +4,47 @@ def jobName = "${owner}-${project}".replaceAll('/','-')
 
 pipelineJob(jobName) {
     definition {
+//        cps {
+//            script(readFileFromWorkspace('Jenkinsfile'))
+//            sandbox()
+//        }
+
         cps {
-            script(readFileFromWorkspace('Jenkinsfile'))
-            sandbox()
+        script('''
+        pipeline {
+    agent any 
+    stages{
+        stage('CleanWorkspace') {
+            steps {
+                cleanWs()
+            }
         }
+        stage('Maven Build'){
+            steps {
+            sh "git clone https://github.com/Sadashiv/maven"
+            sh "cd maven && mvn clean install"
+            }
         }
-//  steps {
-//      shell('mvn clean install')
-//      maven('-e clean install')
-//  }
+        stage('Deploy product'){
+            agent { label 'deploy_server' }
+            steps {
+            echo "Deploy product to desired server"
+            }
+        }
+        stage('Selenium Test'){
+            steps {
+            echo "Run Selenium test case"
+            }
+        }
+        stage('Create docker image '){
+            steps {
+            echo "Docker image creation in progress"
+            }
+        }
+    }
+
+
+}''')
+    }
+  }
 }
