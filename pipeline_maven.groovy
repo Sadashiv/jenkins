30206e12433bd9fc81182293138e753fa2f1a7c2
Sadashiv Badagi <sadamsrit@gmail.com>
2021-05-23 14:08:27 +0530

    Added pipeline job jenkins as code

---
+++ pipeline_maven.groovy
@@ -0,0 +1,17 @@
+def owner = 'sadashiv'
+def project = 'maven_pipeline'
+def jobName = "${owner}-${project}".replaceAll('/','-')
+
+pipelineJob('jobName') {
+    definition {
+        cpsScm {
+            scm {
+                git('https://github.com/${owner}/${project}.git')
+            }
+        }
+    }
+  steps {
+//      shell('mvn clean install')
+      maven('-e clean install')
+  }
+}
